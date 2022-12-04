f6b7dcb089431d252b5793c0c5aad2c3b8d908dd
default-writer <funcelot@outlook.com>
2022-12-04 04:35:21 +0000

    added retry 5 to get gpg keys

--- bin/setup.sh
+++ bin/setup.sh
@@ -29,7 +29,7 @@ then
     gnupg \
     lsb-release
   sudo mkdir -p /etc/apt/keyrings
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+  curl -fsSL --use-ascii --retry 5 --retry-all-errors https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
   echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
