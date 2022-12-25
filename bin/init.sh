4c26b082a09365f76b5a50bacf57cdfb7fd2c140
default-writer <funcelot@outlook.com>
2022-12-25 09:43:29 +0000

    [master] updated init script

--- bin/init.sh
+++ bin/init.sh
@@ -21,10 +21,10 @@ echo "Node version $(node --version)"
 echo "NPM version $(npm --version)"
 echo "Pyenv version $(pyenv --version)"
 
-# sudo "${pwd}/bin/setup.sh" --llvm
-# sudo "${pwd}/bin/setup.sh" --cmake
-# sudo "${pwd}/bin/setup.sh" --pyenv
-# "${pwd}/bin/venv.sh" --pyenv
+sudo "${pwd}/bin/setup.sh" --llvm
+sudo "${pwd}/bin/setup.sh" --cmake
+sudo "${pwd}/bin/setup.sh" --pyenv
+"${pwd}/bin/venv.sh" --pyenv
 
 "${pwd}/bin/runme.sh"
 
