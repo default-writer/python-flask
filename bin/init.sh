9301624058639fe8b4b330828e0b2dec0ce5edbe
default-writer <funcelot@outlook.com>
2022-12-25 09:31:20 +0000

    [master] updated nvm

--- bin/init.sh
+++ bin/init.sh
@@ -15,8 +15,7 @@ pwd=$(pwd)
 
 [ ! $(which nvm) == "" ] && "${pwd}/bin/env.sh" --nvm && "${pwd}/bin/install.sh" --nvm
 [ ! $(which pyenv) == "" ] && "${pwd}/bin/env.sh" --pyenv && "${pwd}/bin/install.sh" --pyenv
-[ ! $(nvm --version) == "19.3.0" ] && nvm install 19.3.0 && nvm use 19.3.0
-[ ! $(npm --version) == "9.2.0" ] && npm install -g npm@9.2.0
+[ ! $(which nvm) == "" ] && nvm install 19.3.0 && nvm use 19.3.0 && npm install -g npm@9.2.0
 
 echo "Node version $(node --version)"
 echo "NPM version $(npm --version)"
