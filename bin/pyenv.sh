35721b9becb2b1588286e70f49f08280928eaf7c
default-writer <funcelot@outlook.com>
2022-12-25 20:02:28 +0300

    [master] fix some weird string

--- bin/pyenv.sh
+++ bin/pyenv.sh
@@ -12,7 +12,7 @@ pwd=$(pwd)
 
 export PYENV_VIRTUALENV_DISABLE_PROMPT=1
 export PYENV_ROOT="$HOME/.pyenv"
-command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 
