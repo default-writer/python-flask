aa62112430f04b23f8841d0a362219cf922afa3d
default-writer <funcelot@outlook.com>
2022-12-26 18:49:10 +0300

    [master] test 2

--- bin/init.sh
+++ bin/init.sh
@@ -7,16 +7,16 @@ pwd=$(pwd)
 "${pwd}/bin/git.sh" --hooks
 
 if [ "$(which nvm)" == "" ]; then
+    eval "$(source ${pwd}/bin/nvm.sh)"
     "${pwd}/bin/install.sh" --nvm
     "${pwd}/bin/env.sh" --nvm
-    eval "$(source $HOME/.bashrc)"
     "${pwd}/bin/venv.sh" --nvm
 fi
 
 if [ "$(which pyenv)" == "" ]; then
+    eval "$(source ${pwd}/bin/pyenv.sh)"
     "${pwd}/bin/install.sh" --pyenv
     "${pwd}/bin/env.sh" --pyenv
-    eval "$(source $HOME/.bashrc)"
     "${pwd}/bin/venv.sh" --pyenv
 fi
 
