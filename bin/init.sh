cc749b842eb9c794aef911380e35b7dfb985ab96
default-writer <funcelot@outlook.com>
2022-12-27 10:47:20 +0300

    [master] turn off nvm

--- bin/init.sh
+++ bin/init.sh
@@ -6,12 +6,12 @@ pwd=$(pwd)
 "${pwd}/bin/git.sh" --git
 "${pwd}/bin/git.sh" --hooks
 
-if [ "$(which nvm)" == "" ]; then
-    eval "$(source ${pwd}/bin/nvm.sh)"
-    "${pwd}/bin/install.sh" --nvm
-    "${pwd}/bin/env.sh" --nvm
-    "${pwd}/bin/venv.sh" --nvm
-fi
+# if [ "$(which nvm)" == "" ]; then
+#     eval "$(source ${pwd}/bin/nvm.sh)"
+#     "${pwd}/bin/install.sh" --nvm
+#     "${pwd}/bin/env.sh" --nvm
+#     "${pwd}/bin/venv.sh" --nvm
+# fi
 
 if [ "$(which pyenv)" == "" ]; then
     eval "$(source ${pwd}/bin/pyenv.sh)"
