61011a1c0a9e420b18c06ebe2ced4d2c0c941336
default-writer <funcelot@outlook.com>
2022-12-28 09:34:25 +0000

    [master] commented out

--- bin/init.sh
+++ bin/init.sh
@@ -6,18 +6,18 @@ pwd=$(pwd)
 "${pwd}/bin/git.sh" --git
 "${pwd}/bin/git.sh" --hooks
 
-# if [ "$(which nvm)" == "" ]; then
-#     eval "$(source ${pwd}/bin/nvm.sh)"
-#     "${pwd}/bin/install.sh" --nvm
-#     "${pwd}/bin/env.sh" --nvm
-#     "${pwd}/bin/venv.sh" --nvm
-# fi
+if [ "$(which nvm)" == "" ]; then
+    eval "$(source ${pwd}/bin/nvm.sh)"
+    "${pwd}/bin/install.sh" --nvm
+    "${pwd}/bin/env.sh" --nvm
+    "${pwd}/bin/venv.sh" --nvm
+fi
 
-# if [ "$(which pyenv)" == "" ]; then
-#     eval "$(source ${pwd}/bin/pyenv.sh)"
-#     "${pwd}/bin/install.sh" --pyenv
-#     "${pwd}/bin/env.sh" --pyenv
-#     "${pwd}/bin/venv.sh" --pyenv
-# fi
+if [ "$(which pyenv)" == "" ]; then
+    eval "$(source ${pwd}/bin/pyenv.sh)"
+    "${pwd}/bin/install.sh" --pyenv
+    "${pwd}/bin/env.sh" --pyenv
+    "${pwd}/bin/venv.sh" --pyenv
+fi
 
 cd "${pwd}"
